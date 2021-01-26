4af7e0ed0874fbf8c38827a3e158381f094a557a
Guillem Ortega <guillem212@hotmail.es>
2021-01-26 14:08:18 +0100

    sounds added

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+
+    [SerializeField] private AudioClip[] music;
+    [SerializeField] private AudioClip[] clicks;
+    [SerializeField] private AudioClip[] combatSounds;
+
+
+    private void Awake() {
+        if(instance is null){
+            instance = this;
+            DontDestroyOnLoad(instance);
+        }
+        else{
+            Destroy(this);
+        }
+    }
+
+
+}
